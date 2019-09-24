7072f73a2818b61c20de33d15af855e021d84b36
eoinahern <eoin.ahern@mycit.ie>
2019-09-24 16:22:12 +0100

    added gcp setup including port

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/eoinahern/migration_app/routes"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	http.Handle("/session", &routes.SessionHandler{})
-	http.ListenAndServe(":8080", nil)
+	http.Handle("/upload", &routes.UploadHandler{})
+	http.Handle("/download", &routes.DownloadHandler{})
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
